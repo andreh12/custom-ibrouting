76300bd1543c397f9a99bee278d07ff232bc21ae
Andre Georg Holzner <aholz@srv-c2c05-22.cms>
2015-05-12 15:00:05 +0200

    added another routing algorithm: class RouteRanking02

--- genRoutes.py
+++ genRoutes.py
@@ -39,6 +39,41 @@ def routeRanking01(occupancyTable, route, sourceLid, destLid):
             occupancyTable.getLeafToSpineCableOccupancy(route),
             occupancyTable.getSpineToLeafCableOccupancy(route))
 
+#----------------------------------------
+class RouteRanking02:
+    def __init__(self, spineSwitchLIDs):
+        # maps from destination LID to LID of spine switch
+        self.buToSpineSwitch = {}
+        self.spineSwitchLIDs = spineSwitchLIDs
+
+    def __call__(self, occupancyTable, route, sourceLid, destLid):
+        spineSwitchLid = route.spineSwitchLid
+
+        if not self.buToSpineSwitch.has_key(destLid):
+            # assign a new spine switch to this destination:
+            # take the spine switch which currently has the fewest routes
+            # going over it
+
+            occupancy, switchLid = min([ (occupancyTable.spineSwitchLIDtoNumRoutes.getCountWithDefault(lid, 0), lid) for lid in self.spineSwitchLIDs ])
+
+            self.buToSpineSwitch[destLid] = switchLid
+
+        switchLid = self.buToSpineSwitch[destLid]
+
+        if switchLid == route.spineSwitchLid:
+            # this is the preferred route
+            spineSwitchRanking = 0
+        else:
+            spineSwitchRanking = 1
+
+        return (
+            spineSwitchRanking,
+
+            # secondary ranking
+            occupancyTable.getSpineToLeafCableOccupancy(route),
+
+            occupancyTable.getLeafToSpineCableOccupancy(route),
+            )
 
 #----------------------------------------------------------------------
 # main
@@ -66,7 +101,13 @@ sourceLids = [ linkData.getLidFromHostname(host) for host in sourceHosts ]
 destLids   = [ linkData.getLidFromHostname(host) for host in destHosts   ]
 
 
-routingAlgo = RoutingAlgo(linkData, sourceLids, destLids, routeRanking01)
+if True:
+    routingAlgo = RoutingAlgo(linkData, sourceLids, destLids, routeRanking01)
+
+if False:
+    routingAlgo = RoutingAlgo(linkData, sourceLids, destLids, None)
+
+    routingAlgo.routeRankingFunc = RouteRanking02(routingAlgo.spineSwitchLIDs)
 
 routingAlgo.run()
 
