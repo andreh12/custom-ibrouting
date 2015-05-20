d4d0f0451654da54ce3845f0256bbc41e5a8d841
Andre Georg Holzner <aholz@srv-C2C03-22.cms>
2015-05-20 12:21:12 +0200

    now reading the list of source and destination hosts from the command line

--- genRoutes.py
+++ genRoutes.py
@@ -15,6 +15,7 @@ from pprint import pprint
 sys.path.append(os.path.expanduser("~aholz/DAQTools/Diagnostics/trunk/network/"))
 
 import iblinkStatusUtils 
+import utils
 
 #----------------------------------------------------------------------
 iblinkInfoExe = "/usr/sbin/iblinkinfo"
@@ -27,12 +28,12 @@ iblinkInfoExe = "/usr/sbin/iblinkinfo"
 
 # 2015-05-12 22x22 from /nfshome0/mommsen/daq2Test/cdaq/canon_1str_22x22/daq2Symbolmap.txt
 
-sourceHosts = ["ru-c2e13-22-01", "ru-c2e13-23-01", "ru-c2e15-19-01", "ru-c2e12-16-01", "ru-c2e12-17-01", "ru-c2e12-18-01", "ru-c2e13-27-01", "ru-c2e13-28-01", "ru-c2e13-29-01", "ru-c2e15-27-01", "ru-c2e12-10-01", "ru-c2e12-11-01", "ru-c2e12-12-01", "ru-c2e12-27-01", "ru-c2e12-28-01", "ru-c2e12-29-01", "ru-c2e14-27-01", "ru-c2e12-13-01", "ru-c2e12-14-01", "ru-c2e12-15-01", "ru-c2e12-24-01", "ru-c2e12-25-01"]
+# sourceHosts = ["ru-c2e13-22-01", "ru-c2e13-23-01", "ru-c2e15-19-01", "ru-c2e12-16-01", "ru-c2e12-17-01", "ru-c2e12-18-01", "ru-c2e13-27-01", "ru-c2e13-28-01", "ru-c2e13-29-01", "ru-c2e15-27-01", "ru-c2e12-10-01", "ru-c2e12-11-01", "ru-c2e12-12-01", "ru-c2e12-27-01", "ru-c2e12-28-01", "ru-c2e12-29-01", "ru-c2e14-27-01", "ru-c2e12-13-01", "ru-c2e12-14-01", "ru-c2e12-15-01", "ru-c2e12-24-01", "ru-c2e12-25-01"]
 
-destHosts = [ "bu-c2f16-17-01", # "bu-c2f16-35-01",
-              "bu-c2e18-35-01", "bu-c2d33-30-01", "bu-c2d41-30-01", "bu-c2d42-30-01",
-              "bu-c2f16-09-01", # "bu-c2f16-31-01",
-              "bu-c2e18-31-01", "bu-c2d33-20-01", "bu-c2d41-20-01", "bu-c2d35-30-01", "bu-c2f16-11-01", "bu-c2e18-11-01", "bu-c2d31-20-01", "bu-c2d36-20-01", "bu-c2e18-23-01", "bu-c2e18-25-01", "bu-c2f13-29-01", "bu-c2f16-13-01", "bu-c2e18-13-01", "bu-c2d31-30-01", "bu-c2d36-30-01", "bu-c2d34-10-01"]
+# destHosts = [ "bu-c2f16-17-01", # "bu-c2f16-35-01",
+#               "bu-c2e18-35-01", "bu-c2d33-30-01", "bu-c2d41-30-01", "bu-c2d42-30-01",
+#               "bu-c2f16-09-01", # "bu-c2f16-31-01",
+#               "bu-c2e18-31-01", "bu-c2d33-20-01", "bu-c2d41-20-01", "bu-c2d35-30-01", "bu-c2f16-11-01", "bu-c2e18-11-01", "bu-c2d31-20-01", "bu-c2d36-20-01", "bu-c2e18-23-01", "bu-c2e18-25-01", "bu-c2f13-29-01", "bu-c2f16-13-01", "bu-c2e18-13-01", "bu-c2d31-30-01", "bu-c2d36-30-01", "bu-c2d34-10-01"]
 
 
 #----------------------------------------------------------------------
@@ -95,21 +96,69 @@ class RouteRanking02:
 #----------------------------------------------------------------------
 # main
 #----------------------------------------------------------------------
-ARGV = sys.argv[1:]
+from optparse import OptionParser
+parser = OptionParser("""
 
-if len(ARGV) == 1:
+    usage: %prog [options] 
+
+    produces tailored Infiniband routing tables
+    """
+    )
+
+parser.add_option("--srcfile",
+                  default = None,
+                  type="str",
+                  help="name of a file with list of source hosts",
+                  metavar="src.txt")
+
+parser.add_option("--destfile",
+                  default = None,
+                  type="str",
+                  help="name of a file with list of destination hosts",
+                  metavar="src.txt")
+
+parser.add_option("--iblinkfile",
+                  default = None,
+                  type="str",
+                  help="name of a file with the output of iblinkinfo",
+                  metavar="src.txt")
+
+(options, ARGV) = parser.parse_args()
+
+
+if options.iblinkfile != None:
     # read the output of iblinkinfo from the given file
-    linkData = iblinkStatusUtils.IBlinkStatusData(open(ARGV[0]).read())
-elif len(ARGV) == 0:
+    linkData = iblinkStatusUtils.IBlinkStatusData(open(options.iblinkfile).read())
+else:
     # run iblinkinfo ourselves
     if not os.path.exists(iblinkInfoExe):
         print "this host does not have " + iblinkInfoExe + ". Are you running on a host connected to the Infiniband network ?"
         sys.exit(1)
 
     linkData = iblinkStatusUtils.IBlinkStatusData(commands.getoutput("/usr/bin/sudo " + iblinkInfoExe))
-else:
-    print >> sys.stderr,"wrong number of command line arguments"
+
+if options.srcfile == None:
+    print >> sys.stderr,"must specify a list of source hosts"
     sys.exit(1)
+else:
+    sourceHosts = utils.readHostsFile(options.srcfile)
+
+
+if options.destfile == None:
+    print >> sys.stderr,"must specify a list of destination hosts"
+    sys.exit(1)
+else:
+    destHosts = utils.readHostsFile(options.destfile)
+    
+
+if not sourceHosts:
+    print >> sys.stderr,"list of source hosts is empty"
+    sys.exit(1)
+
+if not destHosts:
+    print >> sys.stderr,"list of destination hosts is empty"
+    sys.exit(1)
+
 
 #----------------------------------------
 
