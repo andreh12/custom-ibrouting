a99d251e37e278a3d11ece8f522b66db62c73c3d
Andre Georg Holzner <aholz@ru-c2e15-39-01.cms>
2015-07-29 14:57:05 +0200

    initial commit: tool to sort FTS dumps

---
+++ sortFTSdump.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+# sorts the outputs of dump_fts or our own generated routing
+# table so that we can better compare them (the order
+# in which the switches are dumped may be different, rendering
+# a comparison with diff difficult)
+
+import sys, re
+
+# ARGV = sys.argv[1:]
+# assert len(ARGV) == 1
+
+switchToLines = {}
+
+# None means no switch header has been seen so far
+# an empty list means that a switch header line
+# has been seen
+currentSwitchLines = None
+
+currentSwitchName = None
+
+for line in sys.stdin.read().splitlines():
+
+    # example line:
+    #   Unicast lids [0x0-0x1400] of switch DR path slid 0; dlid 0; 0,1,19,32 guid 0xf4521403001d56c0 (MF0;sw-ib-c2f14-44-01:SX6036/U1):
+
+    mo = re.match("Unicast lids \S+ of switch DR path slid \S+; dlid \S+; \S+ guid 0x\S+ \(\S+;(\S+):\S+\):", line)
+
+    if mo:
+        if currentSwitchLines != None and len(currentSwitchLines) > 0:
+            # flush the old switch
+            assert currentSwitchName != None
+
+            assert not switchToLines.has_key(currentSwitchName)
+
+            switchToLines[currentSwitchName] = currentSwitchLines
+
+        # clear
+        currentSwitchLines = [ line ]
+        currentSwitchName = mo.group(1)
+
+        continue
+
+    # just an 'ordinary' line, we must have
+    # seen a header line already
+
+    assert currentSwitchLines != None
+    
+    currentSwitchLines.append(line)
+
+
+# now print them in increasing order of switch name
+
+for switchName in sorted(switchToLines.keys()):
+    for line in switchToLines[switchName]:
+        print line
+
+        
+    
+
+    
+
+
